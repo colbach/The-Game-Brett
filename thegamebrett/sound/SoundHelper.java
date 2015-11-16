6f52763f64ab90bad3acf53c39ef88f2bea4ccfb
colbach <christiancolbach@gmail.com>
2015-11-16 09:35:49 +0100

    Hinzufügen von Kommentare zur übersicht

--- thegamebrett/sound/SoundHelper.java
+++ thegamebrett/sound/SoundHelper.java
@@ -5,14 +5,21 @@ import java.applet.AudioClip;
 import java.net.URL;
 
 /**
- *
+ * Stellt Hilfsmethoden zur Verfuegung. 
+ * 
  * @author Christian Colbach
  */
 public class SoundHelper {
     
+    /*
+    BUGS:
+    - Audiodatei wird stoppt wenn Thread beendet wird.
+    - Loop_Little_Big_Adventure_04.wav kann aus unbekannten Gruenden nicht abgespielt werden.
+    */
+    
     /**
      * Spielt eine Audiodatei ab.
-     * ACHTUNG: Thread muss lange genug laufen damit Datei bis zum Ende abgespielt wird
+     * ACHTUNG: Thread muss lange genug laufen damit Datei bis zum Ende abgespielt wird.
      * 
      * @param resource Resource welche abgespielt werden soll.
      *                 (Pfad zu Datei, ausgehend von src, muss wav sein)
--- thegamebrett/sound/SoundManager.java
+++ thegamebrett/sound/SoundManager.java
@@ -9,6 +9,11 @@ import thegamebrett.model.mediaeffect.SoundEffect;
  */
 public class SoundManager {
 
+    /*
+    TODO:
+    - Etablieren eines Threadsystems zum Management Wiedergabe der Soundelemente.
+    */
+    
     public static void playSoundEffect(SoundEffect se) {
         SoundHelper.playSound(se.getResource());
     }
