6fbc3fdb2cebf2bbe779653eff0b785ada9aa52c
colbach <christiancolbach@gmail.com>
2015-11-23 22:29:27 +0100

    Auslagerung von HTML und CSS

--- src/thegamebrett/network/HTMLGenerator.java
+++ src/thegamebrett/network/HTMLGenerator.java
@@ -42,10 +42,10 @@ public class HTMLGenerator {
         
         StringBuilder sb = new StringBuilder(HTML_TOP + HTML_START);
         sb.append("<div id=\"refresh\" style=\"text-align:center;\"></div>");
-        sb.append(titel);
-        for(int i=0; i<choices.length; i++) {
-            sb.append(generateHTMLButton("", messageID, i));
-        }
+        //sb.append(titel);
+        /*for(int i=0; i<choices.length; i++) {
+            sb.append(generateHTMLButton(choices[i], messageID, i));
+        }*/
         sb.append(HTML_END);
         return sb.toString();
 
@@ -57,6 +57,15 @@ public class HTMLGenerator {
                  //"</div>\n"
                  //"<div align=\"center\"><button class=\"buttons\">v</button></div>"
                  //"<div align=\"center\"><button class=\"choices\" onclick=\"reply('ok')\">Alle ok?</button></div>" + HTML_END;
+    }
+    
+    public static String generateHTMLContent(String titel, String[] choices, int messageID) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<h1>" + titel + "</h1>");
+        for(int i=0; i<choices.length; i++) {
+            sb.append(generateHTMLButton(choices[i], messageID, i));
+        }
+        return sb.toString();
     }
     
     private static String generateHTMLButton(String choice, int messageID, int anwerID) {
@@ -71,5 +80,5 @@ public class HTMLGenerator {
             + "main{ margin: 0 auto; }\n"
             + "#content{ padding:5px; margin-left:5px; }\n"
             + ".buttons{ padding:5px; margin:5px; width:35px; height:35px; }"
-            + ".choices{ padding:5px; margin:5px; width:80%; height:40px; }";
+            + ".choices{ padding:5px; margin:5px; width:80%; height:50px; }";
 }
--- src/thegamebrett/network/test/ControlDirector.java
+++ src/thegamebrett/network/test/ControlDirector.java
@@ -1,27 +1,61 @@
 package thegamebrett.network.test;
 
+import java.net.URL;
 import thegamebrett.network.HTMLGenerator;
 import thegamebrett.network.httpserver.Director;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Scanner;
 
 /**
  * @author Christian Colbach
  */
 public class ControlDirector implements Director {
-
+    
+    public static String HTML = null;
+    static {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL url = classloader.getResource("thegamebrett/network/assets/index.html");
+        try {
+            String content = new Scanner(new File(url.toURI())).useDelimiter("\\Z").next();
+            HTML = content;
+        } catch (URISyntaxException | FileNotFoundException ex) {
+            Logger.getLogger(ControlDirector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static String CSS = null;
+    static {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        URL url = classloader.getResource("thegamebrett/network/assets/style.css");
+        try {
+            String content = new Scanner(new File(url.toURI())).useDelimiter("\\Z").next();
+            CSS = content;
+        } catch (URISyntaxException | FileNotFoundException ex) {
+            Logger.getLogger(ControlDirector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     @Override
     public Object query(String request) throws QueryException {
-        
-        if(request.equals("/") || request.equals("/index.html")) {
-            return HTMLGenerator.generateHTML(null, null);
-        } else if(request.equals("/style.css")) {
-            return HTMLGenerator.CSS;
-        } else if(request.startsWith("/refresh")) {
-            return Test.usertext;
-        } else if(request.startsWith("/reply")) {
+
+        if (request.equals("/") || request.equals("/index.html")) {
+            return HTML;
+        } else if (request.equals("/style.css")) {
+            return CSS;
+        } else if (request.startsWith("/refresh")) {
+            return HTMLGenerator.generateHTMLContent("Wie geht es dir?", new String[]{"gut", "schlecht", "es geht"}, 1001);
+
+            //return Test.usertext;
+        } else if (request.startsWith("/reply")) {
+            
+            
             return "Thanks " + Math.random();
         } else {
             return "Fehler :(";
         }
     }
-    
+
 }
