28bee073d4b0046a552a28bc54d6b58b19b9a0a8
Korekalu <kore.kalu@libo21.de>
2015-11-26 12:29:08 +0100

    letzte changes vorm master ding

--- src/thegamebrett/action/request/InteractionRequest.java
+++ src/thegamebrett/action/request/InteractionRequest.java
@@ -18,7 +18,7 @@ public class InteractionRequest implements GUIRequest, MobileRequest {
         this.choices = choices;
         this.player = player;
         this.hidden = hidden;
-        delay = 0;
+        this.delay = 0;
     }
 
     public InteractionRequest(String titel, String[] choices, Player player, boolean hidden, int delay) {
--- src/thegamebrett/game/menschaergerdichnicht/MADNgameLogic.java
+++ src/thegamebrett/game/menschaergerdichnicht/MADNgameLogic.java
@@ -6,6 +6,7 @@
 package thegamebrett.game.menschaergerdichnicht;
 import thegamebrett.action.ActionRequest;
 import thegamebrett.action.ActionResponse;
+import thegamebrett.action.request.InteractionRequest;
 import thegamebrett.action.response.InteractionResponse;
 import thegamebrett.action.response.TimerResponse;
 import thegamebrett.model.GameLogic;
@@ -39,6 +40,7 @@ public class MADNgameLogic extends GameLogic{
         ActionRequest nextRequest;
         //ein haufen if else anweisungen
         if(as instanceof InteractionResponse){
+            InteractionRequest previous = ((InteractionResponse) as).getConcerningInteractionRequest();
             
         } else if(as instanceof TimerResponse){
             
@@ -50,8 +52,8 @@ public class MADNgameLogic extends GameLogic{
     
     public int dice(){
         //random nr zwischen 1 und 6
-        int random = (int)(Math.random()*6);
-        return random+1;
+        int random = (int)(Math.random()*6)+1;
+        return random;
     }
     
     @Override
@@ -62,7 +64,5 @@ public class MADNgameLogic extends GameLogic{
     @Override
     public int getMinimumPlayers() {
         return minimumPlayers;
-    }
-
-    
+    }  
 }
