f7fc0e30187f9af158f527fbf410b8d0f5c3578a
colbach <christiancolbach@gmail.com>
2015-12-17 15:34:29 +0100

    x

--- src/thegamebrett/network/PlayerNotRegisteredException.java
+++ src/thegamebrett/network/PlayerNotRegisteredException.java
@@ -1,11 +1,21 @@
 package thegamebrett.network;
 
+import thegamebrett.model.Player;
+
 /**
  * Fehler wird geworfen falls Player nicht in System angemeldet ist
  * 
  * @author christiancolbach
  */
 public class PlayerNotRegisteredException extends Exception {
+    
+    private final Player player;
+
+    public PlayerNotRegisteredException(Player player) {
+        this.player = player;
+    }
+            
+            
     
     @Override
     public String getMessage() {
