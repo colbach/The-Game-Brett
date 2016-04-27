b9fbe771c7d353034023648cc1d5b259caf7503e
colbach <christiancolbach@gmail.com>
2016-04-27 19:53:10 +0200

    Merge branch 'master' into christian-branch
    # Conflicts:
    #	BETA 1.1/src/assetsfolder/web/index.html

--- BETA 1.1/src/thegamebrett/assets/AssetsLoader.java
+++ BETA 1.1/src/thegamebrett/assets/AssetsLoader.java
@@ -26,7 +26,8 @@ public class AssetsLoader {
     public static String assetsfolder = null;
 
     static {
-        assetsfolder = System.getProperty("user.home") + "/GitHub/The-Game-Brett/BETA 1.1/src/assetsfolder/";
+        assetsfolder = "/Users/Korè/Documents/Studium/5. Semester(WS)/Interdisziplinäres Teamprojekt/The-Game-Brett/BETA 1.1/src/assetsfolder/";
+        //assetsfolder = System.getProperty("user.home") + "/GitHub/The-Game-Brett/BETA 1.1/src/assetsfolder/";
         System.out.println("assetsfolder = " + assetsfolder);
     }
 
