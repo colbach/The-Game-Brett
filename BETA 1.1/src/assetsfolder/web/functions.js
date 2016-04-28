a7e6871f98159c8d1e0d488d244d5def4398a151
Korekalu <kore.kalu@libo21.de>
2016-04-28 20:38:12 +0200

    seiten weiter

--- BETA 1.1/src/assetsfolder/web/functions.js
+++ BETA 1.1/src/assetsfolder/web/functions.js
@@ -1,3 +1,4 @@
+var backgroundColor = '#B70E79';
 
 $(document).ready(function() {
 	$("#refresh").load("refresh?start");
@@ -9,6 +10,7 @@ $(document).ready(function() {
 function reply(answer) {
 	$("#refresh").load("reply?" + answer);
 }
+
 function startGame(){
 	/*hier muss geprueft werden welches Spiel gewaehlt ist und dieses dann mit allen spielern gestartet werden*/
 	window.location = "ingame.html";
@@ -16,4 +18,11 @@ function startGame(){
 function returnToCharacter(){
 	/*hier muss zum choose character zurück gesprungen werden. wichtig ist, dass falls ein neuer character erstellt wurde, der nun auch zur auswahl steht.*/
 	window.location = "chooseCharacter.html";
+}
+function setBackgroundColor(backgroundcolor){
+	/*hier die farbe für jeden spieler anpassen. gibt es bei google was zu :D*/
+	document.getElementsByTagName("body")[0].style.backgroundColor = backgroundColor;
+}
+function createCharacter(){
+		window.location = "createCharacter.html";
 }
