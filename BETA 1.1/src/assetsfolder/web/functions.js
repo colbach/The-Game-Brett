77bff31823e036b74524fa0a37c8eb267f45ad37
colbach <christiancolbach@gmail.com>
2016-04-29 14:43:53 +0200

    .

--- BETA 1.1/src/assetsfolder/web/functions.js
+++ BETA 1.1/src/assetsfolder/web/functions.js
@@ -1,28 +1,63 @@
 var backgroundColor = '#B70E79';
 
-$(document).ready(function() {
-	$("#refresh").load("refresh?start");
-	var refreshId = setInterval(function() {
-		$("#refresh").load('refresh?' + 1 * new Date());
-	}, 2000);
+$(document).ready(function () {
+    //$("#refresh").load("refresh?start");
+    update();
+    var refreshId = setInterval(function () {
+        update();
+        //$("#refresh").load('refresh?' + 1 * new Date());
+    }, 2000);
 });
 
+var lastContentID = "#########";
+
 function reply(answer) {
-	$("#refresh").load("reply?" + answer);
+    //$("#refresh").load("reply?" + answer);
+    var url = "reply?" + answer;
+    var response = new XMLHttpRequest();
+    response.open("GET", url, true);
+    response.send();
+
+    document.getElementById("refresh").innerHTML = "";
 }
 
-function startGame(){
-	/*hier muss geprueft werden welches Spiel gewaehlt ist und dieses dann mit allen spielern gestartet werden*/
-	window.location = "ingame.html";
+function update() {
+    //var url = 'refresh?' + 1 * new Date();
+    var url = 'refresh?' + lastContentID;
+    var response = new XMLHttpRequest();
+    response.open("GET", url, true);
+    response.send();
+    response.onreadystatechange = function () {
+        if (response.readyState === 4 && response.status === 200) {
+
+            if (response.responseText !== "null") {
+
+                var contentID = response.responseText.substr(0, 9);
+                lastContentID = contentID;
+                var contentText = response.responseText.substr(9);
+
+                document.getElementById("refresh").innerHTML = contentText;
+
+                console.log("update " + contentID);
+            } else {
+                console.log("no update");
+            }
+        }
+    };
 }
-function returnToCharacter(){
-	/*hier muss zum choose character zurück gesprungen werden. wichtig ist, dass falls ein neuer character erstellt wurde, der nun auch zur auswahl steht.*/
-	window.location = "chooseCharacter.html";
+
+function startGame() {
+    /*hier muss geprueft werden welches Spiel gewaehlt ist und dieses dann mit allen spielern gestartet werden*/
+    window.location = "ingame.html";
 }
-function setBackgroundColor(backgroundcolor){
-	/*hier die farbe für jeden spieler anpassen. gibt es bei google was zu :D*/
-	document.getElementsByTagName("body")[0].style.backgroundColor = backgroundColor;
+function returnToCharacter() {
+    /*hier muss zum choose character zurück gesprungen werden. wichtig ist, dass falls ein neuer character erstellt wurde, der nun auch zur auswahl steht.*/
+    window.location = "chooseCharacter.html";
 }
-function createCharacter(){
-		window.location = "createCharacter.html";
+function setBackgroundColor(backgroundcolor) {
+    /*hier die farbe für jeden spieler anpassen. gibt es bei google was zu :D*/
+    document.getElementsByTagName("body")[0].style.backgroundColor = backgroundColor;
+}
+function createCharacter() {
+    window.location = "createCharacter.html";
 }
